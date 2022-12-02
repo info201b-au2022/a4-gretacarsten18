8d457fab7a3f09a9946f9ea61df82ee9b731ca50
Greta Carsten <gretacarsten@MacBook-Air-67.localdomain>
2022-12-02 15:42:02 -0800

    push

--- source/analysis.R
+++ source/analysis.R
@@ -65,13 +65,8 @@ plot_jail_pop_for_us <- function(get_year_jail_pop)  {
     )
   return(plot_jail_pop_data_for_us)   
 } 
-
 plot(plot_jail_pop_for_us(get_year_jail_pop))  
 
-Between the years 1970 and 2018, we have seen a general increase in total incarcerated population
-within the United States. We can see that the lowest that the incarcerated population has been was in the 
-1970s, and the highest was in the 2010s. We can conclude that there has been a significant increase in 
-incarcerated populations, but why has there been such a dramatic increase in jail population? 
   
   
   # TODO: Implement this function 
@@ -103,13 +98,6 @@ plot_jail_pop_by_states <- function(states) {
 
 plot(plot_jail_pop_by_states(c("WA", "NC", "CA", "NY", "FL")))
 
-In this line graph we are looking at the total incarceration population from 1970-2018
-in five states: California, Florida, North Carolina, New York, and Washington. From
-this chart, we can observe a general increase in all five states, except for New York when around
-2000 they start to see a general decrease in total numbers. California has had the highest amounts
-of incarceration, but that is not surprising due to their population being the largest. 
-A question to ask from this chart is was caused New Yorks incarceration decline around
-the 2000s? 
 
 
 
@@ -146,11 +134,7 @@ plot_king_county_data_1970_to_2018 <- function() {
 }
 plot(plot_king_county_data_1970_to_2018())
 
-In this chart we are looking at King County, Washingtons total jail population by gender 
-from 1970-2018. In this chart we can see an increase in jail population for both genders until 
-the 2000s, however, there has been a decline in total numbers since. Another thing we can 
-observe is how the population has a significanlty higher incarcerated male population
-compared to females. Why do men have a much higher rate of incarceration? 
+
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
 # <a map shows potential patterns of inequality that vary geographically>
@@ -175,39 +159,33 @@ us_total_jail_pop_county_state_year_2018 <- us_total_jail_pop_county_state_year 
 
 View(us_total_jail_pop_county_state_year_2018)
 
-jail_pop_ratio <- us_total_jail_pop_county_state_year_2018 %>%
+jail_pop_ratio <- us_total_jail_pop_county_state_year_2018%>%
   select(
     jail_pop_ratio,
     state
   )
 View(jail_pop_ratio)
 
-states_for_plot <- us_total_jail_pop_county_state_year_2018(
-    state = ("CA", "TX", "FL", "NY", "PA", "IL", "OH", "GA", "NC", "MI"),
-    lat = c(36.7783, 31.9686, 27.6648, 40.7128, 41.2033, 40.6331, 40.4173, 32.1656, 35.7596, 44.3148),
-    long = c(119.4179, 99.9018, 81.5158, 74.0060, 77.1945, 89.3985, 82.9071, 82.9001, 79.0193, 85.6024)
-) 
+ #states_for_plot <- us_total_jail_pop_county_state_year_2018(
+    #lat = c(36.7783, 31.9686, 27.6648, 40.7128, 41.2033, 40.6331, 40.4173, 32.1656, 35.7596, 44.3148),
+    #long = c(119.4179, 99.9018, 81.5158, 74.0060, 77.1945, 89.3985, 82.9071, 82.9001, 79.0193, 85.6024)
+ #) 
 
-state_shape <- map_data("state")
-ggplot(state_shape) +
-  geom_polygon(
-    mapping  = aes(x = long, y = lat, state = state), 
-      geom_point(
-        data = jail_pop_ratio,
-        mapping = aes(x = long, y = lat),
-    color = "blue",
-    size = .2
-    caption = "This map is showing the highest populated states with the ratio of their population incarcerated."
-  )) + 
+#state_shape <- map_data("state")
+#ggplot(state_shape) +
+  #geom_polygon(
+    #mapping  = aes(x = long, y = lat, state = state), 
+      #geom_point(
+        #data = jail_pop_ratio,
+        #mapping = aes(x = long, y = lat),
+    #color = "blue",
+    #size = 2,
+    #caption = "This map is showing the highest populated states with the ratio of their population incarcerated."
+  #)) + 
   
-  coord_map()
+#coord_map()
 
-This map is showing the top ten most populated states in the United States, and there
-ratio of population in jail compared to the total population. From this map is it visible 
-that states with the highest populations, for example, California, do not always have the 
-highest percentage of amount of people in jail. For example, Georgia, has the highest
-percentage of people in jail even though it is not the highest populated states. Why
-do some states have a higher ratio of imprisoned people than other states? 
+ 
 
 
 
